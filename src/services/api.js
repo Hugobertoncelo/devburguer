071767b110f28eb570396972c82f52cc166990f8
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-03-13 12:03:35 -0300

    styles carousel

--- src/services/api.js
+++ src/services/api.js
@@ -2,4 +2,12 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "http://localhost:3000"
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+
+  config.headers.authorization = `Bearer ${token}`;
+
+  return config;
 })
