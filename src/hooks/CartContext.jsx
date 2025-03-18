949f8969518f28e8de9a724722b9f066379b9960
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-03-18 10:33:30 -0300

    final cart

--- src/hooks/CartContext.jsx
+++ src/hooks/CartContext.jsx
@@ -98,7 +98,7 @@ export const useCart = () => {
   const context = useContext(CartContext);
 
   if (!context) {
-    throw new Erros('useCart must be used with a context');
+    throw new Error('useCart must be used with a context');
   }
 
   return context;
